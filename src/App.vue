1c85aa1af1476edd4fac35b733dfef48fbde00c0
seunghyukcho <shhj1998@postech.ac.kr>
2021-02-07 17:28:21 +0900

    implemented prototype and publishing

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,47 @@
     >
       <v-toolbar-title>Smart Catheter</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text>
-        <span>Download</span>
-        <v-icon right>mdi-download</v-icon>
-      </v-btn>
-      <v-btn text>
+      <v-btn text href="https://arxiv.org/abs/2102.01932">
         <span>Paper</span>
         <v-icon right>mdi-file</v-icon>
       </v-btn>
-      <v-btn text>
+      <v-btn text href="https://github.com/termlab/smart-catheter">
         <span>Code</span>
         <v-icon right>mdi-code-braces</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols=12 class="text-center">
+            <h1>Smart Catheter</h1>
+            <p>description of dataset</p>
+          </v-col>
+          <v-col cols=12>
+            <h2>How did we collected?</h2>
+            <p>description</p>
+          </v-col>
+          <v-col cols=12>
+            <h2>Validation</h2>
+            <p>description</p>
+          </v-col>
+          <v-col cols=12>
+            <h2>Download</h2>
+            <p>description</p>
+            <v-row>
+              <v-btn class="mx-3">
+                <span>Train data</span>
+                <v-icon right>mdi-download</v-icon>
+              </v-btn>
+              <v-btn>
+                <span>Test data</span>
+                <v-icon right>mdi-download</v-icon>
+              </v-btn>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -29,7 +55,10 @@
 <script>
 export default {
   name: 'App',
-
+  metaInfo: {
+    title: 'Smart Catheter',
+    titleTemplate: '%s'
+  },
   data: () => ({
     //
   }),
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import VueMeta from 'vue-meta'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+Vue.use(VueMeta)
 
 new Vue({
   vuetify,
